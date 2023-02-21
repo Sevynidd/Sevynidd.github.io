ca294297b9ba61329f53fb32677a71951439daa6
PapsFranziskus <rhys.tedeschi@web.de>
2023-02-21 07:59:22 +0100

    Wenn auf einen der Buttons in der NavBar geklickt wird, werden alle anderen nicht mehr gehighlighted nur noch der geklickte

--- Code/user.js
+++ Code/user.js
@@ -1,3 +1,30 @@
+const Dashboard_Button = document.getElementById("btn-dashboard");
+const ticketCreate_Button = document.getElementById("btn-ticketCreate");
+const tickets = document.getElementById("btn-tickets");
+const styles = getComputedStyle(document.body);
+const highlightColor = styles.getPropertyValue("--highlight");
+
+var Buttons = [
+    document.getElementById("btn-dashboard"),
+    document.getElementById("btn-ticketCreate"),
+    document.getElementById("btn-tickets")
+];
+
 window.onload = function() {
-    getDarkMode(); 
+    getDarkMode();
+    resetHighlight(); 
 }
+for (let i = 0; i < Buttons.length; i++) {
+    Buttons[i].addEventListener("click", function(event){  
+        resetHighlight();     
+        Buttons[i].style.background = highlightColor;   
+    });
+}
+  
+
+function resetHighlight(){    
+    for (var i = 0; i < Buttons.length; i++) {
+        Buttons[i].style.background = '';
+    } 
+}
+
