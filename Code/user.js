110ab6a5f5e9285b44ab2cd88de2e6dc4cc5f59c
PapsFranziskus <rhys.tedeschi@web.de>
2023-02-21 09:13:45 +0100

    Buttons umstrukturiert und die Funktionalität des Darkmode_Buttons hinzugefügt

--- Code/user.js
+++ Code/user.js
@@ -1,9 +1,8 @@
-const Dashboard_Button = document.getElementById("btn-dashboard");
-const ticketCreate_Button = document.getElementById("btn-ticketCreate");
-const tickets = document.getElementById("btn-tickets");
 const styles = getComputedStyle(document.body);
 const highlightColor = styles.getPropertyValue("--highlight");
+const dark_mode_button = document.getElementById("btn-darkmode");
 
+//Liste der Buttons für funktionalitäten auf der Navbar, DarkmodeButton ausgeschlossen
 var Buttons = [
     document.getElementById("btn-dashboard"),
     document.getElementById("btn-ticketCreate"),
@@ -21,7 +20,6 @@ window.onload = function() {
     setButtonText();
     resetHighlight();
     Buttons[0].style.background = highlightColor;
-
 }
 
 for (let i = 0; i < Buttons.length; i++) {
@@ -40,6 +38,11 @@ function resetHighlight(){
 function setButtonText(){
     for (var i = 0; i < Buttons.length; i++) {
         Buttons[i].querySelector('#btn-text').textContent = ButtonsText[i];
-        }
+    }
 }
+
+dark_mode_button.addEventListener("click", function(event){
+    toggleDarkmode();
+})
+
 
