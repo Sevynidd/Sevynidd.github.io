353701aacaa90631dfcb39374781b5bc9de7d538
Sevynidd <Sevynidd02@protonmail.com>
2023-02-23 15:10:53 +0100

    Code Formatting

--- Code/darkmode.js
+++ Code/darkmode.js
@@ -5,7 +5,7 @@ var icons = [
     document.getElementById("btn-dashboard"),
     document.getElementById("btn-ticketCreate"),
     document.getElementById("btn-tickets")
-    
+
 ]
 
 function getDarkMode() {
@@ -20,26 +20,26 @@ function getDarkMode() {
     }
 }
 
-function toggleDarkmode(){
+function toggleDarkmode() {
     // Icon des Darkmode-Button entsprechend ändern
-    if (!document.body.classList.contains("dark_mode")){
-        if (icons[0]){
-            icons[0].classList.replace("fa-moon","fa-sun"); 
-            saveDarkmode("true");   
+    if (!document.body.classList.contains("dark_mode")) {
+        if (icons[0]) {
+            icons[0].classList.replace("fa-moon", "fa-sun");
+            saveDarkmode("true");
         }
     }
     else {
-        if (icons[0]){
-            icons[0].classList.replace("fa-sun","fa-moon");
-            saveDarkmode("false");  
+        if (icons[0]) {
+            icons[0].classList.replace("fa-sun", "fa-moon");
+            saveDarkmode("false");
         }
-    }    
+    }
     document.body.classList.toggle("dark_mode");
     // Die Icons inversen sodass sie weiss sind wenn der Darkmode aktiv ist
 
-    for (var i=0; i<=icons.length; i++){
-        if (icons[i]){
-            icons[i].classList.toggle("fa-inverse"); 
+    for (var i = 0; i <= icons.length; i++) {
+        if (icons[i]) {
+            icons[i].classList.toggle("fa-inverse");
         }
     }
 }
