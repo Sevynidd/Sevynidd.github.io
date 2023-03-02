84e91c7cdd0d2436412ff15b520a10f621366627
PapsFranziskus <rhys.tedeschi@web.de>
2023-03-02 10:27:13 +0100

    TinyMCE aus der .html genommen und in die php gepackt

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
+<script src="https://cdn.tiny.cloud/1/be2pkyay8feepi47nibdlaeih6m32kbe832fyazmdadurnd7/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>tinymce.init({selector:'textarea'});</script>
+  
     <head>
         <title>Ticket Create</title>
     </head>
     <body>
         <h1 style="color: #ffffff">TicketCreate</h1>
+
+        <textarea></textarea>
     </body>
 </html>
