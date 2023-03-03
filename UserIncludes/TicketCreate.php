2f4b1da9da4f7dd3adadef48428a056ae4fef8b3
PapsFranziskus <rhys.tedeschi@web.de>
2023-03-03 10:25:58 +0100

    TinyMCE mit Quill ausgetauscht

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
-<script type="text/javascript" src="https://cdn.tiny.cloud/1/be2pkyay8feepi47nibdlaeih6m32kbe832fyazmdadurnd7/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-<script> tinymce.init({selector:'textarea'});</script>
-<link rel="stylesheet" type="text/css" id="mce-u0" crossorigin="anonymous" referrerpolicy="origin" href="https://cdn.tiny.cloud/1/be2pkyay8feepi47nibdlaeih6m32kbe832fyazmdadurnd7/tinymce/6.3.2-13/skins/ui/oxide/skin.min.css">
 <head>
-        <title>Ticket Create</title>
+    <title>Ticket Create</title> 
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
     </head>
     <body>
         <h1 style="color: #ffffff">TicketCreate</h1>  
-        <textarea></textarea>
+        <div id="editor"></div>
+        <script>
+             var quill = new Quill('#editor', {
+                theme: "snow"
+            });
+        </script>
     </body>
 </html>
