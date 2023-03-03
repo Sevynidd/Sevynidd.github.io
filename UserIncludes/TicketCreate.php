4adbabf9b87b5edf3e2eb4ef24af4bcf0c4bac16
Sevynidd <sevynidd02@protonmail.com>
2023-03-03 13:21:24 +0100

    Verschieben des TicketCreate.css Dokuments

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -3,7 +3,7 @@
 <head>
     <title>Ticket Create</title> 
     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-    <link rel="stylesheet" href="/UserIncludes/TicketCreate.css" type="text/css">
+    <link rel="stylesheet" href="/UserIncludes/Style/TicketCreate.css" type="text/css">
     <link rel="stylesheet" href="/Style/global.css" type="text/css">
     <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
     </head>
