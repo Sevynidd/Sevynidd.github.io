319da78d402ebd748c5344ecf6f724e432304b7d
Sevynidd <sevynidd02@protonmail.com>
2023-03-03 10:51:49 +0100

    TextField white background

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -7,12 +7,15 @@
     </head>
     <body>
         <h1 style="color: #ffffff">TicketCreate</h1>  
-        <div id="editor"></div>
-        <script>
-             var quill = new Quill('#editor', {
-                theme: "snow"
-            });
-        </script>
+        <div style="background: white; width: 1500px; height: 500px;">
+        <div id="editor" style="border: none"></div>
+            <script>
+                var quill = new Quill('#editor', {
+                    theme: "snow"
+                });
+            </script>
+        </div>
+        
         <button class="btn" id="btn-submit" type="submit"> Speichern</button>
     </body>
 </html>
