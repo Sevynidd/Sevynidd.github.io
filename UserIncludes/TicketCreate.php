65d9f2ef11686d184a87d6475ef56757ffb42b83
PapsFranziskus <rhys.tedeschi@web.de>
2023-03-07 09:05:58 +0100

    container für die einzelnen Elemente + Abbrechen- Button hinzugefügt und aufgehübscht

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -9,47 +9,51 @@
     </head>
     <body>
         <h1>TicketCreate</h1> <br><br><br>
-        <label for="betreff">Betreff</label><br><br>
-        <input type="text" list="dliste" id="betreff" placeholder="Betreff"/>
-        <datalist id="dliste">
-            <option value="Aachener …">
-            <option value="Hamburger …">
-            <option value="Mitteldeutscher …">
-            <option value="Niederrheinische …">
-        </datalist><br>
-        <label for="prio">Priorität</label><br>
-        <select name="priority" id="prio">
-            <option>Work in Progress</option>
-        </select>
-        <label for="cat">Kategorie</label><br>
-        <select name="category" id="cat">
-            <option>Work in Progress</option>
-        </select>
-        <div class="background-color-editor">
-            <div id="editor"></div>            
-                <script>
-                    var quill = new Quill('#editor', {
-                        modules: {
-                            toolbar: [
-                                // eine eckige Klammer repräsentiert einen "abschnitt"
-                                // dadurch entsteht ein gewisser abstand zu anderen elementen
-                                    [{ 'font': [] }],
-                                    ['bold', 'italic', 'underline', 'strike'],
-                                    ['blockquote', 'code-block'],
-                                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                                    [{ 'list': 'ordered'}, { 'list': 'bullet' }],     
-                                    [{ 'indent': '-1'}, { 'indent': '+1' }],        
-                                    [{ 'direction': 'rtl' }],  
-                                    [{ 'color': [] }, { 'background': [] }], 
-                                    [{ 'align': [] }],
-                                    ['image', 'link']
-                                ]
-                        },
-                        placeholder: 'Beschreibung zu deinem Problem',
-                        theme: 'snow'  
-                    });
-                </script>
-        </div>        
-        <button id="btn-submit" type="submit">Speichern</button>
+
+        <div class="container">
+            <label for="betreff">Betreff</label><br><br>
+            <input type="text" list="dliste" id="betreff" placeholder="Betreff"/>
+            <datalist id="dliste">
+                <option value="Aachener …">
+                <option value="Hamburger …">
+                <option value="Mitteldeutscher …">
+                <option value="Niederrheinische …">
+            </datalist><br>
+            <label for="prio">Priorität</label><br>
+            <select name="priority" id="prio">
+                <option>Work in Progress</option>
+            </select>
+            <label for="cat">Kategorie</label><br>
+            <select name="category" id="cat">
+                <option>Work in Progress</option>
+            </select>
+            <div class="background-color-editor">
+                <div id="editor"></div>            
+                    <script>
+                        var quill = new Quill('#editor', {
+                            modules: {
+                                toolbar: [
+                                    // eine eckige Klammer repräsentiert einen "abschnitt"
+                                    // dadurch entsteht ein gewisser abstand zu anderen elementen
+                                        [{ 'font': [] }],
+                                        ['bold', 'italic', 'underline', 'strike'],
+                                        ['blockquote', 'code-block'],
+                                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],     
+                                        [{ 'indent': '-1'}, { 'indent': '+1' }],        
+                                        [{ 'direction': 'rtl' }],  
+                                        [{ 'color': [] }, { 'background': [] }], 
+                                        [{ 'align': [] }],
+                                        ['image', 'link']
+                                    ]
+                            },
+                            placeholder: 'Beschreibung zu deinem Problem',
+                            theme: 'snow'  
+                        });
+                    </script>
+            </div>        
+            <button id="btn-submit" type="submit">Speichern</button>
+            <button id="btn-cancel" type="submit">Abbrechen</button>
+        </div>
     </body>
 </html>
