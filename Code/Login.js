b8ca7e7a87f6764395866758e0ee7d023d7d7c6d
PapsFranziskus <rhys.tedeschi@web.de>
2023-03-08 09:44:25 +0100

    Passwort anzeigen/verbergen möglich

--- Code/Login.js
+++ Code/Login.js
@@ -35,3 +35,18 @@ PasswortEdit.addEventListener("keypress", function (event) {
         LogIn();
     }
 });
+
+$(document).ready(function() {
+    $("#password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#password input').attr("type") == "text"){
+            $('#password input').attr('type', 'password');
+            $('#password i').addClass( "fa-eye-slash" );
+            $('#password i').removeClass( "fa-eye" );
+        }else if($('#password input').attr("type") == "password"){
+            $('#password input').attr('type', 'text');
+            $('#password i').removeClass( "fa-eye-slash" );
+            $('#password i').addClass( "fa-eye" );
+        }
+    });
+});
