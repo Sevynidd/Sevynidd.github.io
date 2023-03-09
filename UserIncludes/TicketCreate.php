6ab46770d97a73681be46e61f3489ca1ba15af89
Sevynidd <sevynidd02@protonmail.com>
2023-03-09 08:56:43 +0100

    Betreff pflicht

--- UserIncludes/TicketCreate.php
+++ UserIncludes/TicketCreate.php
@@ -14,95 +14,97 @@
 
 <body>
     <div class="container mx-4 px-5 py-5">
-        <div class="row mb-1">
-            <div class="input-group">
-                <span class="input-group-text">Betreff</span>
-                <input type="text" class="form-control" id="Betreff" placeholder="Betreff" autocomplete="off"
-                    list="BetreffListe">
-                <datalist id="BetreffListe">
-                    <option value="Audi"></option>
-                    <option value="Lambo"></option>
-                    <option value="BMW"></option>
-                </datalist>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-md-4">
+        <form method="post">
+            <div class="row mb-1">
                 <div class="input-group">
-                    <span class="input-group-text">Priorität</span>
-                    <select class="form-select" id="Prioritaet">
-                        <option value="1" selected>Kann weiterarbeiten</option>
-                        <option value="2">Kann nicht weiterarbeiten</option>
-                    </select>
+                    <span class="input-group-text">Betreff</span>
+                    <input type="text" class="form-control" id="Betreff" placeholder="Betreff" autocomplete="off"
+                        list="BetreffListe" required>
+                    <datalist id="BetreffListe">
+                        <option value="Audi"></option>
+                        <option value="Lambo"></option>
+                        <option value="BMW"></option>
+                    </datalist>
                 </div>
             </div>
-            <div class="col-md-2">
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" name="checkboxVisible" id="checkboxVisible">
-                    <label class="form-check-label" for="checkboxVisible">Ticket für alle sichtbar</label>
+            <div class="row my-4">
+                <div class="col-md-4">
+                    <div class="input-group">
+                        <span class="input-group-text">Priorität</span>
+                        <select class="form-select" id="Prioritaet">
+                            <option value="1" selected>Kann weiterarbeiten</option>
+                            <option value="2">Kann nicht weiterarbeiten</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="checkboxVisible" id="checkboxVisible">
+                        <label class="form-check-label" for="checkboxVisible">Ticket für alle sichtbar</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="input-group">
+                        <span class="input-group-text">Kategorie</span>
+                        <select class="form-select" id="Kategorie">
+                            <option value="1" selected>Vemas</option>
+                            <option value="2">PC-Probleme</option>
+                            <option value="3">Neue Hardware - Anfrage</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="input-group">
-                    <span class="input-group-text">Kategorie</span>
-                    <select class="form-select" id="Kategorie">
-                        <option value="1" selected>Vemas</option>
-                        <option value="2">PC-Probleme</option>
-                        <option value="3">Neue Hardware - Anfrage</option>
-                    </select>
+            <div class="row mt-5">
+                <div class="background-color-editor">
+                    <div id="editor"></div>
+                    <script>
+                    var quill = new Quill('#editor', {
+                        modules: {
+                            toolbar: [
+                                // eine eckige Klammer repräsentiert einen "abschnitt"
+                                // dadurch entsteht ein gewisser abstand zu anderen elementen
+                                [{
+                                    'font': []
+                                }],
+                                ['bold', 'italic', 'underline', 'strike'],
+                                ['blockquote', 'code-block'],
+                                [{
+                                    'header': [1, 2, 3, 4, 5, 6, false]
+                                }],
+                                [{
+                                    'list': 'ordered'
+                                }, {
+                                    'list': 'bullet'
+                                }],
+                                [{
+                                    'indent': '-1'
+                                }, {
+                                    'indent': '+1'
+                                }],
+                                [{
+                                    'direction': 'rtl'
+                                }],
+                                [{
+                                    'color': []
+                                }, {
+                                    'background': []
+                                }],
+                                [{
+                                    'align': []
+                                }],
+                                ['image', 'link']
+                            ]
+                        },
+                        placeholder: 'Beschreibung zu deinem Problem',
+                        theme: 'snow'
+                    });
+                    </script>
                 </div>
+            
             </div>
-        </div>
-        <div class="row mt-5">
-            <div class="background-color-editor">
-                <div id="editor"></div>
-                <script>
-                var quill = new Quill('#editor', {
-                    modules: {
-                        toolbar: [
-                            // eine eckige Klammer repräsentiert einen "abschnitt"
-                            // dadurch entsteht ein gewisser abstand zu anderen elementen
-                            [{
-                                'font': []
-                            }],
-                            ['bold', 'italic', 'underline', 'strike'],
-                            ['blockquote', 'code-block'],
-                            [{
-                                'header': [1, 2, 3, 4, 5, 6, false]
-                            }],
-                            [{
-                                'list': 'ordered'
-                            }, {
-                                'list': 'bullet'
-                            }],
-                            [{
-                                'indent': '-1'
-                            }, {
-                                'indent': '+1'
-                            }],
-                            [{
-                                'direction': 'rtl'
-                            }],
-                            [{
-                                'color': []
-                            }, {
-                                'background': []
-                            }],
-                            [{
-                                'align': []
-                            }],
-                            ['image', 'link']
-                        ]
-                    },
-                    placeholder: 'Beschreibung zu deinem Problem',
-                    theme: 'snow'
-                });
-                </script>
-            </div>
-           
-        </div>
-        <button class="btn btn-primary" id="btn-submit" type="submit">Ticket erstellen</Button>  
-        <button class="btn btn-primary" id="btn-cancel" type="submit">Inhalt löschen</Button>  
+            <button class="btn btn-primary" id="btn-submit" type="submit">Ticket erstellen</Button>  
+            <button class="btn btn-primary" id="btn-cancel" type="submit">Inhalt löschen</Button>  
+        </form>
     </div>
 </body>
 
